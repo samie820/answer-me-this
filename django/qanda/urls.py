6d17478a5a6db87df7ca50acbb37be0784744250
samie820 <samuelomole70@gmail.com>
2019-01-03 01:07:05 +0100

    Added the new views for qanda

--- django/qanda/urls.py
+++ django/qanda/urls.py
@@ -1,9 +1,16 @@
 from django.urls.conf import path
 
-from .views import AskQuestionView
+from .views import AskQuestionView, QuestionDetailView, CreateAnswerView, UpdateAcceptanceAnswer, DailyQuestionList
 
 app_name = 'qanda'
 
 urlpatterns = [
-    path('ask', AskQuestionView.as_view(), name='ask')
+    path('ask', AskQuestionView.as_view(), name='ask'),
+    path('q/<int:pk>', QuestionDetailView.as_view(), name='question_detail'),
+    path('q/<int:pk>/answer', CreateAnswerView.as_view(), name='answer_question'),
+    path('q/<int:pk>/accept', UpdateAcceptanceAnswer.as_view(),
+         name='update_answer_acceptance'),
+    path('daily/<int:year>/<int:month>/<int:day>/',
+         DailyQuestionList.as_view(), name='daily_questions')
+
 ]
